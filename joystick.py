4f30092797464d0237e5094ad099c27c347dfb8f
618knot <knot618@gmail.com>
2024-09-17 17:35:00 +0900

    joystickクラスの用意

--- joystick.py
+++ joystick.py
@@ -1,16 +1,38 @@
-from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup([20, 21], GPIO.IN)
 
-try:
-    while True:
-        input20 = GPIO.input(20)
-        input21 = GPIO.input(21)
-        print(f"20: {input20}, 21: {input21}")
-        sleep(1)
-except:
-    GPIO.cleanup()
-    print("stop")
+X_PIN = 20
+Y_PIN = 21
+
+
+
+# テスト用。後で消す
+# from time import sleep
+# try:
+#     while True:
+#         input_x = GPIO.input(X_PIN)
+#         input_y = GPIO.input(Y_PIN)
+#         print(f"x: {input_x}, y: {input_y}")
+#         sleep(1)
+# except:
+#     GPIO.cleanup()
+#     print("stop")
+
+class Joystick:
+    # default values
+    input_x = 0
+    input_y = 0
+    
+    def __init__(self) -> None:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup([X_PIN, Y_PIN], GPIO.IN)
+        
+    def get_values():
+        input_x = GPIO.input(X_PIN)
+        input_y = GPIO.input(Y_PIN)
+        
+        return { "x": input_x, "y": input_y }
+
+    def cleanup():
+        GPIO.cleanup()
